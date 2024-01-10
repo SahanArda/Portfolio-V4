4ae6c253e6f40cd28dc922546ed681ed1ec4c0c3
Arda Sahan <ardambp@outlook.com>
2024-01-10 00:15:15 +0000

    converted webm to mp4

--- src/components/Blackhole/Blackhole.jsx
+++ src/components/Blackhole/Blackhole.jsx
@@ -3,7 +3,7 @@ import styles from "./Blackhole.module.css";
 const Blackhole = () => {
   return (
     <video autoPlay muted loop playsInline className={styles.blackhole}>
-      <source src="/blackhole.webm" type="video/webm" />
+      <source src="/blackhole.mp4" type="video/mp4" />
     </video>
   );
 };
