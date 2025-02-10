b6013214d9b94202842ae1db2037dd4dee9d5d6e
Arda Sahan <ardambp@outlook.com>
2025-02-10 18:45:25 +0000

    updated project cards with up to date projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -5,34 +5,40 @@ import styles from "./Projects.module.css";
 const Projects = () => {
   const projects = [
     {
-      title: "Teletherapy Platform",
-      description: "Fullstack application that connects therapists and patients for virtual consultations. This platform features secure user authentication, profile management, appointment scheduling. Designed to facilitate remote mental health support.",
-      technologies: "ReactJs Tailwind NodeJs ExpressJs Typescript PostgreSQL Docker",
-      githubLink: "https://github.com/OAProjects/MASS"
+      title: "Task Manager",
+      description: "The Task Manager Application is a full-stack web application designed to help users efficiently manage their tasks. Built with a modern tech stack, it offers a seamless experience for task creation, editing, and deletion, all secured through robust authentication and authorisation techniques.",
+      technologies: "React Tailwind NodeJs ExpressJs Sequalize MySQL",
+      githubLink: "https://github.com/SahanArda/Task-Manager-App"
     },
     {
-      title: "Resume Scanner",
-      description: "Python fullstack project which takes users resume and job description as input and matches the skills in the resume with the job description. The result is displayed as a percentage match.",
-      technologies: "ReactJs Tailwind Python Flask",
-      githubLink: "https://github.com/SahanArda/Resume-Scanner"
+      title: "Minimum Spanning Tree (MST) Visualiser",
+      description: "A Python-based CLI tool to construct graphs, compute their Minimum Spanning Tree (MST), and visualise both the original graph and the MST side by side. The project uses NetworkX, Matplotlib, and Typer for an interactive and intuitive experience.",
+      technologies: "Python OOP",
+      githubLink: "https://github.com/SahanArda/MST-Visualiser"
+    },
+    {
+      title: "Speed Type",
+      description: "The Speed Typing Application is a full-stack web application designed to test and improve users' typing speed and accuracy. The application generates a random paragraph on each refresh, and users must type it as accurately and quickly as possible. Upon completion, they receive a real-time score reflecting their words per minute (WPM) and accuracy.",
+      technologies: "React Tailwind Python Flask PostgreSQL",
+      githubLink: "https://github.com/SahanArda/Speed-Type"
+    },
+    {
+      title: "Portfolio Website V2",
+      description: "Second iteration crafted with precision using ReactJS, Bootstrap, and Sass. The seamless design not only reflects my proficiency but also provides visitors with a visual journey through my skills, projects, and professional journey, offering a comprehensive glimpse into my capabilities.",
+      technologies: "React Sass Bootstrap",
+      githubLink: "https://github.com/SahanArda/Portfolio-V4"
     },
     {
       title: "Bookshelf",
       description: "Fullstack application where users can login or register and add the books they are currently reading to their digital bookshelf.",
-      technologies: "ReactJs Sass Bootstrap NodeJs NestJs MongoDB",
+      technologies: "React Sass Bootstrap NodeJs NestJs MongoDB",
       githubLink: "https://github.com/SahanArda/Bookshelf-App"
     },
     {
       title: "ProjectShare",
       description: "A social media platform where users can share their projects and get feedback from other users.",
-      technologies: "ReactJs MaterialUI NodeJs ExpressJs MongoDB",
+      technologies: "React MaterialUI NodeJs ExpressJs MongoDB",
       githubLink: "https://github.com/SahanArda/ProjectShare"
-    },
-    {
-      title: "Portfolio Website V2",
-      description: "Second iteration crafted with precision using ReactJS, Bootstrap, and Sass. The seamless design not only reflects my proficiency but also provides visitors with a visual journey through my skills, projects, and professional journey, offering a comprehensive glimpse into my capabilities.",
-      technologies: "ReactJs Sass Bootstrap",
-      githubLink: "https://github.com/SahanArda/Portfolio-V4"
     },
     {
       title: "Portfolio Website V1",
